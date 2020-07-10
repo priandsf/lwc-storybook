5e23a4abac3f01bff6bc29e4b66d594530acc088
Philippe Riand <priand@salesforce.com>
2020-07-10 13:45:50 -0400

    Added custom element registration

--- packages/lwc-library/src/hello/greetings/greetings.js
+++ packages/lwc-library/src/hello/greetings/greetings.js
@@ -1,17 +1,19 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, wire, buildCustomElementConstructor } from 'lwc';
 
 import { Time } from '../../wire/time'
+
 /**
  * Greetings component that shows what an LWC component can provide.
  */
-export default class Complex extends LightningElement {
+export default class Greetings extends LightningElement {
 
     @api name
 
     @wire(Time) time;
 
-    //@track
     get greetings() {
         return `Glad to see you, ${this.name}`
     }
 }
+
+customElements.define("hello-greetings", buildCustomElementConstructor(Greetings));
--- packages/lwc-library/src/hello/world/world.js
+++ packages/lwc-library/src/hello/world/world.js
@@ -1,7 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, buildCustomElementConstructor } from 'lwc';
 
 /**
  * The simpliest, possible Hello World component.
  */
 export default class World extends LightningElement {
 }
+
+customElements.define("hello-world2", buildCustomElementConstructor(World));
